0dcad223b79e3a3b04473561472e8219ca7bebef
Hasan Turken <turkenh@gmail.com>
2021-10-31 22:43:16 +0300

    Add provider config init for plugin-sdk <v2
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

--- config/provider.go
+++ config/provider.go
@@ -9,7 +9,10 @@ const resourcePrefix = "template"
 
 // GetProvider returns provider configuration
 func GetProvider() tjconfig.Provider {
-	pc := tjconfig.NewProvider(tf.Provider(), resourcePrefix, "github.com/crossplane-contrib/provider-tf-template")
+	pc := tjconfig.NewProvider(tf.Provider().ResourcesMap, resourcePrefix, "github.com/crossplane-contrib/provider-tf-template")
+	// Comment out the line below instead of the above, if your Terraform
+	// provider uses an old version (<v2) of github.com/hashicorp/terraform-plugin-sdk.
+	// pc := tjconfig.NewProvider(tjconfig.GetV2ResourceMap(tf.Provider()), resourcePrefix, "github.com/crossplane-contrib/provider-tf-template")
 
 	for _, configure := range []func(provider tjconfig.Provider){
 		// add custom config functions
