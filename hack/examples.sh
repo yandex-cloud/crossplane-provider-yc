06b89f99f432031e5150b50fb5dbc14900b66511
Evgeny Kuznetsov <eugenschmidt@yandex-team.ru>
2024-01-31 23:25:26 +0300

    CLOUD-167255: don't test all examples

---
+++ hack/examples.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -aeuo pipefail
+
+function join {
+  local f=${1-}
+  if shift; then
+    printf %s "$f" "${@/#/,}"
+  fi
+}
+
+# providerconfig gets created on initializing tests,
+# testing folder requires cloud admin privileges, not feasible
+# securitygroup and securitygrouprule can be created, but not really altered (yet?), so don't pass tests
+# iam/serviceaccountkey has a bug: https://nda.ya.ru/t/8HkjK_y074Vmc4
+# storage/object needs investigation
+# storage/bucket does get removed, but Crossplane receives Forbidden; needs investigation
+all=$(find ${1} -name "*.yaml" \
+-not -path "*/alb/*" \
+-not -path "*/compute/*" \
+-not -path "*/container/*" \
+-not -path "*/datatransfer/*" \
+-not -path "*/dns/*" \
+-not -path "*/kms/*" \
+-not -path "*/kubernetes/*" \
+-not -path "*/mdb/*" \
+-not -path "*/message/*" \
+-not -path "*/vpc/securitygroup*.yaml" \
+-not -path "*/iam/serviceaccountkey.yaml" \
+-not -path "*/storage/object.yaml" \
+-not -path "*/storage/bucket.yaml" \
+-not -path "*/providerconfig/providerconfig.yaml" \
+-not -path "*/resourcemanager/folder.yaml")
+
+join $all
