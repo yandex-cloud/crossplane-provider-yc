b7d760af56896c6bc4bc32ad154c691317ad10a0
Evgeny Kuznetsov <eugenschmidt@yandex-team.ru>
2024-01-31 19:28:53 +0300

    CLOUD-167255: disable k8s/nodegroup and message/queue tests

--- hack/examples.sh
+++ hack/examples.sh
@@ -13,10 +13,11 @@ function join {
 # testing folder requires cloud admin privileges, not feasible
 # container/repository needs registry ID explicitly provided
 # datatransfer/transfer actually runs upon creation
+# message/queue works, but cleaning it in case of tests failure is a problem
 
 ## CURRENTLY FAILING:
-# securitygroup and securitygrouprule can be created, but not really altered (yet?), so don't pass tests
 # iam/serviceaccountkey has a bug: https://nda.ya.ru/t/8HkjK_y074Vmc4
+# kubernetes/nodegroup needs investigation
 # storage/object needs investigation
 # storage/bucket does get removed, but Crossplane receives Forbidden; needs investigation
 # securitygroup and securitygrouprule can be created, but not really altered (yet?), so don't pass tests
@@ -24,9 +25,11 @@ all=$(find ${1} -name "*.yaml" \
 -not -path "*/alb/*" \
 -not -path "*/container/repository.yaml" \
 -not -path "*/datatransfer/transfer.yaml" \
+-not -path "*/message/queue.yaml" \
 -not -path "*/mdb/*" \
 -not -path "*/vpc/securitygroup*.yaml" \
 -not -path "*/iam/serviceaccountkey.yaml" \
+-not -path "*/kubernetes/nodegroup.yaml" \
 -not -path "*/storage/object.yaml" \
 -not -path "*/storage/bucket.yaml" \
 -not -path "*/providerconfig/providerconfig.yaml" \
