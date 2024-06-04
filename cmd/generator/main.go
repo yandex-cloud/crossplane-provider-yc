2d8de555f8919b68ddcbb5a0169503fd1b0e3a5c
Evgeny Kuznetsov <eugenschmidt@yandex-team.ru>
2024-06-04 11:24:08 +0300

    CLOUD-173919: fix license headers

--- cmd/generator/main.go
+++ cmd/generator/main.go
@@ -1,9 +1,14 @@
 /*
-Copyright 2021 The Crossplane Authors.
+Copyright 2024 YANDEX LLC
+This is modified version of the software, made by the Crossplane Authors
+and available at: https://github.com/crossplane-contrib/provider-jet-template
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
