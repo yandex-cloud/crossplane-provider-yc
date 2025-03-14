0591e2472f455ab9bbcebd8f8e07b3bafdb09577
eugenschmidt <eugenschmidt@yandex-team.com>
2025-03-14 12:39:13 +0300

    log version in upbound publishing attempt commit_hash:a369bd3469103c9b484774a5240c954f0ede6192

--- hack/publish_upbound.sh
+++ hack/publish_upbound.sh
@@ -4,4 +4,10 @@ echo "##teamcity[blockOpened name='keys' description='set up credentials']"
 make up-login
 echo "##teamcity[blockClosed name='keys']"
 
+if [[ -z "$VERSION" ]]; then
+  echo "VERSION is not set"
+  exit 1
+fi
+
+echo "Publishing version $VERSION"
 make publish REGISTRY_ORGS=xpkg.upbound.io/yandexcloud XPKG_REG_ORGS=xpkg.upbound.io/yandexcloud
